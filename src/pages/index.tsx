be45093a1ea94c828853da03dd6206da3ce30ef3
Antoni Górski <antoni.gorski@int.pl>
2023-05-21 14:30:10 +0200

    Add main gallery skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Layout } from "../components/layout";
 
 export default function Home() {
+  const articles = getArticles();
+
   return (
     <Layout>
-      content
+      <h1>Check our latest posts</h1>
+
+      <Gallery>
+        {articles.map((article) => {
+          return (
+            <GalleryArticle key={article.id} article={article}/>
+          );
+        })}
+      </Gallery>
     </Layout>
   )
 }
