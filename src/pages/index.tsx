bdf4cbdbf9e488eed015eea6f2054ed8c1f8a602
Antoni Górski <antoni.gorski@int.pl>
2023-05-22 19:24:39 +0200

    Add articles sorting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,24 @@ import { GalleryArticle } from "src/components/layout/Gallery/GalleryArticle/Gal
 import { useArticles } from "src/hooks/use-articles";
 import styles from './index.module.sass'
 import Link from "next/link";
+import { useMemo } from "react";
 
 export default function Home() {
   const articles = useArticles();
+
+  const sortedArticles = useMemo(
+    () => articles.sort((article1, article2) => {
+      return article1.timestamp < article2.timestamp ? 1 : -1;
+    }),
+    [articles]
+  );
 
   return (
     <Layout>
       <h1 className={styles.heading}>Check our latest posts</h1>
 
       <Gallery>
-        {articles.map((article) => {
+        {sortedArticles.map((article) => {
           return (
             <Link href={`/articles/${article.id}`} key={article.id}>
               <GalleryArticle article={article}/>
