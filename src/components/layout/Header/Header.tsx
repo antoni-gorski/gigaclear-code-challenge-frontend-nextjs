df12ca6aafb48bb51d60bad8a41e0dd44d99fd61
Antoni Górski <antoni.gorski@int.pl>
2023-05-22 18:25:57 +0200

    Add navigation

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Logo } from '@/atoms/Logo';
 import styles from './Header.module.sass'
+import Link from 'next/link';
 
 export const Header: React.FC= () => {
   return (
   <header className={styles.header}>
     <div className={styles.container}>
-      <Logo type='core'/>
+      <Link href="/">
+        <Logo type='core'/>
+      </Link>
     </div>
   </header>
   )
